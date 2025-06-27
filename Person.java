14e4501cf529b332f3d82e6a0658d435e8e9c711
Ramesh-hari <ramesh.harikrishnan.h@gmail.com>
2025-06-27 17:19:36 +0530

    Add files via upload
    Question 4 , part-1

--- Person.java
+++ Person.java
@@ -1,49 +1,24 @@
-package task.two;
-
 import java.util.Scanner;
 
 public class Person {
-        // 1.1 Create a class person with properties(name and age)?
+      // 1.4 page-1 Define a base class Person with attributes name, age
 
-        String name;
-        int age;
+      String name ;
+      int age ;
 
-        // Default constructor
-        // a. Default age of person should be 18
-        Person() {
-            age = 18;
-        }
+      // Parameterized constructor
+      Person(String name, int age){
+          this.name = name;
+          this.age = age;
+      }
 
-        // Parameterized constructor
-        // b. A person object can be initialized with name and age
-        Person(String _name, int _age) {
-            name = _name;
-            age = _age;
-        }
+      // Main method
+    public static void main(String[] args) {
 
-        // c. Method to display name and age of the person
-        void displayInfo() {
-            System.out.println("Name: " + name);
-            System.out.println("Age: " + age);
-        }
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        int age = scanner.nextInt();
 
-        // Main method
-        public static void main(String[] args) {
-            Person sample = new Person();
-            int num = sample.age;
-
-            // Instruction to user
-            System.out.println("Write a name :");
-
-            // Get input from user
-            Scanner input1 = new Scanner(System.in);
-            String inputName = input1.next();
-
-            Person person = new Person(inputName,num);
-            // Display the details
-            person.displayInfo(); //Output: User input name and input age
-
-        }
+        Person person = new Person(name , age);
     }
-
-
+}
